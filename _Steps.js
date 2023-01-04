cd28ac503446b09d1d23141c363c0874acd2e2b8
unknown <SoumenBaral>
2023-01-05 02:04:04 +0600

    staps add

---
+++ _Steps.js
@@ -0,0 +1,16 @@
+/**
+ * 1.Create a folder (manually or mkdir)
+ * 2.Open Command Line to that Folder 
+ * 3.npm init
+ * 4.npm install express
+ * 5.Create index.js
+ * ...................
+ * ..........Express/node Server.......
+ * 1. Require express
+ * 2. Create app Variable 
+ * 3. Declare Port 
+ * 4. Set app.get('/')
+ * 5. Listen to port 
+ * 6. Node index.js
+ * 7. Check your Browser for Port 
+ * * */
